672a051d77bc193ce963960f200529124506c183
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-21 15:44:31 -0600

    chore: Update global CSS and layout for improved styling and favicon support

---
+++ plms/src/app/Data/User.ts
@@ -0,0 +1,8 @@
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export default User;
--- plms/src/app/layout.tsx
+++ plms/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <a href="/favicon.ico" rel="icon" />
+      </Head>
+      <body className="bg-slate-300">{children}</body>
     </html>
   );
 }
