47bfcd8e29d04dbf93a9976263618a995be691ad
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-28 13:43:07 -0600

    `chore: Update SecondaryHeader component with mobile menu functionality`

--- plms/src/app/components/SecondaryHeader.tsx
+++ plms/src/app/components/SecondaryHeader.tsx
@@ -51,7 +51,7 @@ export default function SecondaryHeader() {
           <h1 className="text-5xl font-bold leading-6 text-gray-900">Topics</h1>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-md font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
@@ -62,8 +62,8 @@ export default function SecondaryHeader() {
           <div className="fixed inset-0 z-50"></div>
           <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+              <a href="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">PLUMS</span>
                 <Image
                   className="h-8 w-auto"
                   src="/imgs/logo.png"
