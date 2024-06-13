b96ee2bcba233dced978f891c006c1b0e9e99220
kerokero014 <kevinjrm.kr@gmail.com>
2024-06-13 14:00:14 -0600

    `chore: Update layout.tsx with Inter font import`

--- plms/src/app/layout.tsx
+++ plms/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
